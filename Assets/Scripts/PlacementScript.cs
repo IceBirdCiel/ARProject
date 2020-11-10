6db7cec2a96afdc467bfe1e9143a07918ef9efdb
Céline <cceline74@hotmail.fr>
2020-11-10 19:38:26 +0100

    First Version, can actually place the presse.fbx on a horizontal surface using AR on Android device

---
+++ Assets/Scripts/PlacementScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlacementScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objToSpawn;
+
+    [SerializeField]
+    private GameObject spawner;
+
+    private bool isSpawned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSpawned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            if (!isSpawned)
+            {
+                objToSpawn = Instantiate(objToSpawn, spawner.transform.position, spawner.transform.rotation);
+                isSpawned = true;
+            }
+            else
+            {
+                objToSpawn.transform.position = spawner.transform.position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class SpawnerManager : MonoBehaviour
+{
+    [SerializeField]
+    public ARRaycastManager raycast;
+
+    [SerializeField]
+    public GameObject spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        raycast.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        if(hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+        }
+    }
+}
